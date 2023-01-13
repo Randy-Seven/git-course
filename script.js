b74b042c0237541881ebd540a1ba59085c9ec2c1
Vlad06013 <81689910+Vlad06013@users.noreply.github.com>
2023-01-13 11:32:34 +0700

    добавил консольлог

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.body.innerHTML = '<h1>Hello from javascript</h1>';
 console.log('test2rtrgrtgrtgrtg');
 console.log('testfromhome');
 console.log('ewofewiofjewf');
+console.log('ewofewiofjewfaaaa');
